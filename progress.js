f7f22a8f37b701df4a60d601d457815eb17dfd0f
Jayasurya Tatikonda <jtatikon@sfu.ca>
2022-04-30 16:16:06 -0700

    new working

--- progress.js
+++
@@ -1,9 +0,0 @@
-function updateProgressBar(progressBar, value){
-  value = Math.round(value);
-  progressBar.querySelector(".progress-fill").style.width = `${value}%`;
-  progressBar.querySelector(".progress-text").textContent = `${value}%`;
-}
-
-const myProgressBar = document.querySelector(".progress");
-
-updateProgressBar(myProgressBar, 72);   //how to update progressbar example number 
