12c3ad562d5bd62d88bf927f9c942291cc88764c
Andrew Ang <andrewangx@gmail.com>
2022-04-30 03:48:51 -0700

    add image mountain + test task list

--- progress.js
+++ progress.js
@@ -7,3 +7,20 @@ function updateProgressBar(progressBar, value){
 const myProgressBar = document.querySelector(".progress");
 
 updateProgressBar(myProgressBar, 72);   //how to update progressbar example number 
+
+
+
+
+const taskList = document.getElementsByClassName("list-group-item"); 
+console.log(taskList);
+console.log(taskList.length);   // ???? does not work gives 0 for collection
+console.log(taskList.item(0));
+
+var task = taskList[0].getElementsByTagName("input"); // ???? does not work  should have give first item on list
+
+// const task = taskList[0].getElementsByTagName("input")[0].checked;
+console.log(task);
+
+
+function completion(){
+}
